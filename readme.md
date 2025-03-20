9ac84d22480becb894809a763f0c2f6a6364085a
noellimx <67155581+noellimx@users.noreply.github.com>
2025-03-20 17:36:04 +0800

    +src

---
+++ readme.md
@@ -0,0 +1,5 @@
+# ECR
+
+repository for:
+1. source code
+2. script to push to ECR
---
+++ src/main.go
@@ -0,0 +1,18 @@
+package src
+
+import (
+	"fmt"
+	"math/rand/v2"
+	"net/http"
+	"os"
+)
+
+func main() {
+	foo := os.Getenv("FOO")
+	serverRandom := rand.IntN(1000)
+	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
+		w.Write([]byte(fmt.Sprintf("Server Random: %d, FOO=%s", serverRandom, foo)))
+		w.WriteHeader(http.StatusOK)
+	})
+	http.ListenAndServe(":80", nil)
+}
