edb969ba007e6401e0c7befe688dbaeb3458ef71
noellimx <67155581+noellimx@users.noreply.github.com>
2025-03-21 11:39:53 +0800

    -

--- deploy/push.sh
+++ deploy/push.sh
@@ -6,3 +6,6 @@ echo "dirname/readlink: $( dirname -- "$( readlink -f -- "$0"; )"; )"
 tempdir="$( dirname -- "$( readlink -f -- "$0"; )"; )/temp"
 
 echo "${tempdir}"
+
+mkdir $tempdir
+rm -rf $tempdir
