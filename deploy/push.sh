bbd7b515a41861b61f5f2416f1783de58f5c785f
noellimx <67155581+noellimx@users.noreply.github.com>
2025-03-21 12:21:50 +0800

    -

--- deploy/push.sh
+++ deploy/push.sh
@@ -1,20 +1,17 @@
 #/bin/bash
 
-
 echo "dirname/readlink: $( dirname -- "$( readlink -f -- "$0"; )"; )"
 
 tempdir="$( dirname -- "$( readlink -f -- "$0"; )"; )/temp"
+ecrrepo="692859948557.dkr.ecr.us-east-1.amazonaws.com/awesomedev"
 
 echo "${tempdir}"
-
 mkdir $tempdir
 cd $tempdir
 git clone https://github.com/aws-ome-dev/ecr.git
-ls
+git checkout main
 cd ./ecr
-ls ./deploy
-docker build -f ./deploy/Dockerfile .
-
+docker build -t $ecrrepo:latest  -f ./deploy/Dockerfile .
 
 rm -rf $tempdir
 
--- readme.md
+++ readme.md
@@ -1,5 +1,11 @@
 # ECR
 
-repository for:
-1. source code
-2. script to push to ECR
+# directories
+1. source code `./src`
+2. script to push to ECR `./deploy`
+   - The script is independent of the source code. copy `./deploy` to a new directory to execute deployment.
+
+
+# ECR
+
+Repository: `692859948557.dkr.ecr.us-east-1.amazonaws.com/awesomedev`
