017b84ae47d18396c295455e4e0be765e20fb706
noellimx <67155581+noellimx@users.noreply.github.com>
2025-03-21 12:23:36 +0800

    -

--- deploy/push.sh
+++ deploy/push.sh
@@ -4,14 +4,14 @@ echo "dirname/readlink: $( dirname -- "$( readlink -f -- "$0"; )"; )"
 
 tempdir="$( dirname -- "$( readlink -f -- "$0"; )"; )/temp"
 ecrrepo="692859948557.dkr.ecr.us-east-1.amazonaws.com/awesomedev"
-
+image="$ecrrepo/latest"
 echo "${tempdir}"
 mkdir $tempdir
 cd $tempdir
 git clone https://github.com/aws-ome-dev/ecr.git
 git checkout main
 cd ./ecr
-docker build -t $ecrrepo:latest  -f ./deploy/Dockerfile .
-
+docker build -t $image  -f ./deploy/Dockerfile .
+docker push $image
 rm -rf $tempdir
 
