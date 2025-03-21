e8c51d1172ee0759c27c8a6e0725354d36c2ee6b
noellimx <67155581+noellimx@users.noreply.github.com>
2025-03-21 12:37:39 +0800

    -

--- src/main.go
+++ src/main.go
@@ -13,7 +13,6 @@ func main() {
 	serverRandom := rand.IntN(1000)
 	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
 		w.Write([]byte(fmt.Sprintf("Server Random: %d, FOO=%s", serverRandom, foo)))
-		w.WriteHeader(http.StatusOK)
 	})
 	log.Println("serving https server...")
 	http.ListenAndServe(":80", nil)
