d85754fc6857869426127029b10f23a052f73399
noellimx <67155581+noellimx@users.noreply.github.com>
2025-03-21 12:28:02 +0800

    -

--- deploy/push.sh
+++ deploy/push.sh
@@ -1,16 +1,30 @@
 #/bin/bash
 
-echo "dirname/readlink: $( dirname -- "$( readlink -f -- "$0"; )"; )"
-
+# defaults
 tempdir="$( dirname -- "$( readlink -f -- "$0"; )"; )/temp"
 ecrrepo="692859948557.dkr.ecr.us-east-1.amazonaws.com/awesomedev"
+
+# overwrites
+while getopts f:t:c: flag; do
+    case "${flag}" in
+        e) ecrrepo=${OPTARG} ;;
+    esac
+done
+
 image="$ecrrepo/latest"
+
+echo "dirname/readlink: $( dirname -- "$( readlink -f -- "$0"; )"; )"
+
+
 echo "${tempdir}"
 mkdir $tempdir
 cd $tempdir
 git clone https://github.com/aws-ome-dev/ecr.git
 git checkout main
 cd ./ecr
+
+aws ecr get-login-password --region us-east-1 | docker login --username AWS --password-stdin 692859948557.dkr.ecr.us-east-1.amazonaws.com
+
 docker build -t $image  -f ./deploy/Dockerfile .
 docker push $image
 rm -rf $tempdir
--- readme.md
+++ readme.md
@@ -5,6 +5,12 @@
 2. script to push to ECR `./deploy`
    - The script is independent of the source code. copy `./deploy` to a new directory to execute deployment.
 
+# Deployment Requirements
+
+- go 1.23
+- docker cli
+- ECR Repository and Credentials
+
 
 # ECR
 
